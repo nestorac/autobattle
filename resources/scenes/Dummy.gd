2189b8c13ae117de6ae12966d6d3dc7d9d2a6598
Néstor <nestorac@gmail.com>
2023-06-16 19:48:31 +0200

    Now the menu is on the way...

--- resources/scenes/Dummy.gd
+++ resources/scenes/Dummy.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
+@onready var damage_label = $DamageLabel
+
 @export var life = 50
 
 func damage(_damage):
 	life -= _damage
 	print ("damage!!!")
+	damage_label.play_animation(_damage)
 	if life <= 0:
 		queue_free()
---
+++ resources/scripts/ui.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@onready var label_green = $LabelGreen
+@onready var label_red = $LabelRed
+
+@onready var slider_green = $SliderGreen
+@onready var slider_red = $SliderRed
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	label_green.text = "Number of green units: " + str(slider_green.value)
+	label_red.text = "Number of red units: " + str(slider_red.value)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_slider_green_drag_ended(value_changed):
+	label_green.text = "Number of green units: " + str(slider_green.value)
+
+
+func _on_slider_red_drag_ended(value_changed):
+	label_red.text =  "Number of red units: " + str(slider_red.value)
